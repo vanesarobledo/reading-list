410e10a00222c9625ed7b2a3cb0e3a2571fa6e98
Vanesa Robledo <v.robledo@vehikl.com>
2026-07-20 16:38:40 -0400

    Add Status enum

---
+++ app/Enums/Status.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Enums;
+enum Status: string
+{
+    case WANT_TO_READ = 'want_to_read';
+    case READING = 'reading';
+    case FINISHED = 'finished';
+    case ABANDONED = 'abandoned';
+}
