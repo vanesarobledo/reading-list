55eb2c75b5a8c692ea2e4b376377c23bf30a0933
Vanesa Robledo <v.robledo@vehikl.com>
2026-07-20 17:01:37 -0400

    WIP add form to add a book to reading list

---
+++ resources/js/pages/AddToReadingList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1 class="text-2xl mb-4">Add to Reading List</h1>
+    <form>
+        <fieldset class="flex flex-col gap-2 rounded-lg border border-neutral-200 p-3">
+            <legend class="text-lg font-bold">Add a Book</legend>
+            <div class="grid-cols-2">
+                <label>Title</label>:
+                <input
+                    type="text"
+                    class="rounded-md border border-neutral-300"
+                />
+            </div>
+            <div class="grid-cols-2">
+                <label>Author</label>:
+                <input
+                    type="text"
+                    class="rounded-md border border-neutral-300"
+                />
+            </div>
+        </fieldset>
+    </form>
+</template>
