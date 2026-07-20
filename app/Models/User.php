aaf0afc657f2d4f3300f45b36ce9f964407718cb
Vanesa Robledo <v.robledo@vehikl.com>
2026-07-20 16:39:35 -0400

    Add relationship to books and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -42,5 +43,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function books(): BelongsToMany
+    {
+        return $this->belongstoMany(Book::class)
+            ->withPivot(['status', 'started_at', 'finished_at']);
     }
 }
