b174035d2abd1bd1304d63a4f32bc68d00da43d9
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-06 10:22:14 -0400

    add test to add a book

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+    Route::post('/book', [BookController::class, 'store'])->name('book.store');
 });
 
 require __DIR__.'/settings.php';
