9bd3b69d60dbd5731db2f375eadab0362ab9e4a4
Vanesa Robledo <v.robledo@vehikl.com>
2026-09-08 10:59:01 -0400

    add function to create a reading log entry

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['title', 'author', 'publisher', 'publication_year', 'isbn', 'external_source', 'external_id'];
 }
