d5b02ee51783d805cde5e51162fd69d7003a3834
Vanesa Robledo <v.robledo@vehikl.com>
2026-09-09 14:19:48 -0400

    create BookResource

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'isbn' => $this->isbn,
+            'publication_year' => $this->publication_year,
+        ];
+    }
+}
