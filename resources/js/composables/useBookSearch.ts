de767fdb96a855e8f1c232fc62ea318d918b9518
Vanesa Robledo <v.robledo@vehikl.com>
2026-09-10 16:43:37 -0400

    create book search composable

---
+++ resources/js/composables/useBookSearch.ts
@@ -0,0 +1,97 @@
+import { refDebounced } from '@vueuse/core';
+import type { ComputedRef, Ref } from 'vue';
+import { computed } from 'vue';
+import { ref, watch } from 'vue';
+import { index } from '@/actions/App/Http/Controllers/BookController';
+import type { Book, PaginatedResponse } from '@/types';
+
+const MIN_QUERY_LENGTH = 2;
+const PER_PAGE = '5';
+
+export type UseBookSearchReturn = {
+    results: Ref<Array<Book>>;
+    loading: Ref<boolean>;
+    error: Ref<string | null>;
+    belowMinimumLength: ComputedRef<boolean>;
+    noResults: ComputedRef<boolean>;
+};
+export function useBookSearch(): UseBookSearchReturn {
+    const searchQuery = ref('');
+    const searchResults: Ref<Array<Book>> = ref([]);
+    const debounced = refDebounced(searchQuery, 300);
+    const loading = ref(false);
+    const error: Ref<string | null> = ref(null);
+    const belowMinimumLength = computed(() => {
+        return searchQuery.value.length < MIN_QUERY_LENGTH;
+    });
+    const noResults = computed(() => {
+        return (
+            searchResults.value.length === 0 &&
+            !belowMinimumLength.value &&
+            !loading.value &&
+            !error.value
+        );
+    });
+    const abortController: Ref<AbortController | null> = ref(null);
+
+    async function fetchResults() {
+        if (abortController.value) {
+            abortController.value.abort();
+            console.log('Fetch results aborted');
+        }
+
+        const fetchAbortController: AbortController = new AbortController();
+
+        const url: string = index.url({
+            query: {
+                per_page: PER_PAGE,
+                search: searchQuery.value,
+            },
+        });
+
+        loading.value = true;
+        error.value = null;
+
+        try {
+            abortController.value = fetchAbortController;
+            const signal: AbortSignal = abortController.value.signal;
+            const response: Response = await fetch(url, { signal });
+            const result: PaginatedResponse<Book> = await response.json();
+            console.log(result);
+            searchResults.value = result.data;
+        } catch (e: unknown) {
+            if (abortController.value === fetchAbortController) {
+                // TODO: Better error messages
+                if (e instanceof Error) {
+                    console.error(e.message);
+                    error.value = e.message;
+                } else {
+                    error.value = 'Error retrieving search results';
+                }
+            }
+        } finally {
+            if (abortController.value === fetchAbortController) {
+                loading.value = false;
+            }
+        }
+    }
+
+    watch(
+        () => debounced.value,
+        async () => {
+            if (searchQuery.value.length < MIN_QUERY_LENGTH) {
+                return;
+            }
+
+            await fetchResults();
+        },
+    );
+
+    return {
+        results: searchResults,
+        loading: loading,
+        error: error,
+        belowMinimumLength: belowMinimumLength,
+        noResults: noResults,
+    };
+}
